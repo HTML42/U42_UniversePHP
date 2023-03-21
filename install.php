6cecc9fe1bb5addeac902ab85363ae066a638d01
Mark Paspirgilis <mp@power-of-m.de>
2023-03-22 00:41:33 +0100

    Added install script and .gitignore file

---
+++ install.php
@@ -0,0 +1,30 @@
+<?php
+// Create environment file
+if (!file_exists('../env')) {
+    file_put_contents('../env', 'DEV');
+} else {
+    echo "Environment file already exists.";
+    exit;
+}
+
+// Create necessary directories
+$dirs = ['_cache', 'controller', 'handler', 'supermodels', 'views'];
+foreach ($dirs as $dir) {
+    if (!file_exists('../' . $dir)) {
+        mkdir('../' . $dir);
+        file_put_contents('../' . $dir . '/.gitkeep', '');
+    }
+}
+
+// Create .gitignore files in necessary directories
+$ignores = ['_cache'];
+foreach ($ignores as $ignore) {
+    if (!file_exists('../' . $ignore . '/.gitignore')) {
+        file_put_contents('../' . $ignore . '/.gitignore', "*\n!.gitignore");
+    }
+}
+
+// Create .gitignore file in root directory
+if (!file_exists('../.gitignore')) {
+    file_put_contents('../.gitignore', "_cache/\n");
+}
