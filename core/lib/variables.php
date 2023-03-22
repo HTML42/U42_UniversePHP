0673627dd3b18f103f9b539a9daebfff36593480
Mark Paspirgilis <mp@power-of-m.de>
2023-03-22 01:03:18 +0100

    basic php files and classes

--- core/lib/variables.php
+++ core/lib/variables.php
@@ -5,7 +5,7 @@ define('DIR_FRAMEWORK', str_replace('\\', '/', dirname(dirname(__DIR__))) . '/')
 define('DIR_PROJECT', str_replace('\\', '/', dirname(dirname(dirname(__DIR__)))) . '/');
 define('DIR_CORE', DIR_FRAMEWORK . '/core/');
 define('DIR_PLUGINS', DIR_FRAMEWORK . '/plugins/');
-define('DIR_CLASSES', DIR_FRAMEWORK . 'classes/');
+define('DIR_CLASSES', DIR_CORE . 'classes/');
 define('DIR_LIB', DIR_CORE . 'lib/');
 define('DIR_HANDLERS', DIR_PROJECT . '/handlers/');
 define('DIR_MODELS', DIR_PROJECT . '/models/');
