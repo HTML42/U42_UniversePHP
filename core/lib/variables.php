9d27d162904826b0ebc0962dc820026d2bc5f75b
Mark Paspirgilis <mp@power-of-m.de>
2023-03-23 00:59:56 +0100

    execute and basics

--- core/lib/variables.php
+++ core/lib/variables.php
@@ -17,6 +17,13 @@ define('HOUR', 3600);
 define('DAY', HOUR * 24);
 define('WEEK', DAY * 7);
 
+require_once(DIR_CLASSES . 'utilities.class.php');
+require_once(DIR_CLASSES . 'request.class.php');
+require_once(DIR_CLASSES . 'response.class.php');
+require_once(DIR_CLASSES . 'supermodel.class.php');
+require_once(DIR_CLASSES . 'universe.class.php');
+require_once(DIR_CLASSES . 'file.class.php');
+
 $routes = array();
 $controller_files = glob(DIR_CONTROLLER . '/*.json');
 foreach ($controller_files as $controller_file) {
@@ -41,3 +48,13 @@ foreach (glob(DIR_SUPERMODELS . '*.supermodel.php') as $handler_class_filepath) 
 define('FILE_ENVIRONMENT', DIR_PROJECT . 'env');
 
 define('ENV', is_file(FILE_ENVIRONMENT) ? strtolower(trim(file_get_contents(FILE_ENVIRONMENT))) : 'DEV');
+
+// Set asset prefix based on requested URL path
+$asset_prefix = '';
+for ($i = 0; $i < count(Request::$requested_clean_path_array) - 1; $i++) {
+    $asset_prefix .= '../';
+}
+define('ASSET_PREFIX', $asset_prefix);
+
+// Define base URL
+define('BASEURL', 'http' . (is_https() ? 's' : '') . '://' . $_SERVER['SERVER_NAME'] . '/' . Request::$url_path_to_script);
--- execute.php
+++ execute.php
@@ -2,23 +2,6 @@
 
 include __DIR__ . DIRECTORY_SEPARATOR . 'core' . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'variables.php';
 
-require_once(DIR_CLASSES . 'utilities.class.php');
-require_once(DIR_CLASSES . 'request.class.php');
-require_once(DIR_CLASSES . 'response.class.php');
-require_once(DIR_CLASSES . 'supermodel.class.php');
-require_once(DIR_CLASSES . 'universe.class.php');
-require_once(DIR_CLASSES . 'file.class.php');
-
-// Set asset prefix based on requested URL path
-$asset_prefix = '';
-for ($i = 0; $i < count(Request::$requested_clean_path_array) - 1; $i++) {
-    $asset_prefix .= '../';
-}
-define('ASSET_PREFIX', $asset_prefix);
-
-// Define base URL
-define('BASEURL', 'http' . (is_https() ? 's' : '') . '://' . $_SERVER['SERVER_NAME'] . '/' . Request::$url_path_to_script);
-
 // Match requested route and redirect if necessary
 $route = Universe::match_route(ROUTES, Request::$requested_clean_path);
 if (isset($route['redirect']) && is_string($route['redirect'])) {
